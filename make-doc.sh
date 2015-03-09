0bf91c8f7bddfac8902193f2f72acedd74dd8641
anzhu <anzhu@cnord.ru>
2015-03-09 12:23:31 +0300

    Подключение к компьютеру, кривая верстка

--- make-doc.sh
+++ make-doc.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
-pandoc --template tex/nord-template.latex -V lang=russian -o nord-manual.pdf nord/01-00-arm-general.md nord/01-01-arm-tm-key.md nord/01-02-arm-rx-6.md nord/01-03-arm-rxn-400.md nord/01-04-arm-wrl-key.md nord/01-05-arm-myalarm.md nord/02-on-board-indication.md
+echo "Start to make Nord manual"
 
-pandoc --template tex/sergeant-template.latex -V lang=russian -o sergeant-manual.pdf sergeant/01-01-purpose.md sergeant/01-02-specification.md sergeant/01-03-package.md sergeant/01-04-composition.md sergeant/01-05-function.md
+pandoc --template tex/nord-template.latex -V lang=russian -o nord-manual.pdf nord/03-00-arm-general.md nord/03-01-arm-tm-key.md nord/03-02-arm-rx-6.md nord/03-03-arm-rxn-400.md nord/03-04-arm-wrl-key.md nord/03-05-arm-myalarm.md nord/05-00-config-general.md nord/05-01-config-driver.md
+
+echo "Stop to make Nord manual"
+
+
+# echo "Start to make Sergeant manual"
+
+# pandoc --template tex/sergeant-template.latex -V lang=russian -o sergeant-manual.pdf sergeant/01-01-purpose.md sergeant/01-02-specification.md sergeant/01-03-package.md sergeant/01-04-composition.md sergeant/01-05-function.md
+
+# echo "Stop to make Sergeant manual"
