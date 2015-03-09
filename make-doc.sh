fb0a7266880b8b6ba9c4de3b9213536d4dfe2bb4
anzhu <anzhu@cnord.ru>
2015-03-09 15:14:44 +0300

    Обновление версии в кривой верстке
    Обновление версии - готово.
    В подключении к компьютеру добавлен пункт о необходимости внешнего
    питания.

--- make-doc.sh
+++ make-doc.sh
@@ -2,7 +2,7 @@
 
 echo "Start to make Nord manual"
 
-pandoc --template tex/nord-template.latex -V lang=russian -o nord-manual.pdf nord/03-00-arm-general.md nord/03-01-arm-tm-key.md nord/03-02-arm-rx-6.md nord/03-03-arm-rxn-400.md nord/03-04-arm-wrl-key.md nord/03-05-arm-myalarm.md nord/05-00-config-general.md nord/05-01-config-driver.md
+pandoc --template tex/nord-template.latex -V lang=russian -o nord-manual.pdf nord/03-00-arm-general.md nord/03-01-arm-tm-key.md nord/03-02-arm-rx-6.md nord/03-03-arm-rxn-400.md nord/03-04-arm-wrl-key.md nord/03-05-arm-myalarm.md nord/05-00-config-general.md nord/05-01-config-driver.md nord/05-02-config-version.md
 
 echo "Stop to make Nord manual"
 
