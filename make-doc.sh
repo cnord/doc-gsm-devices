2caaa26b9173aefcd606d03d918827768f9de0ff
anzhu <anzhu@cnord.ru>
2015-04-26 14:20:09 +0300

    Описание вкладки «Радио»
    Есть ощущение, что про формат станции нужно написать подробнее.

--- make-doc.sh
+++ make-doc.sh
@@ -2,9 +2,9 @@
 
 echo "Start to make Nord manual"
 
-pandoc --template tex/nord-template.latex -V lang=russian -o nord-manual.pdf nord/01-01-tech-desc.md nord/01-02-features.md nord/01-03-specification.md nord/01-04-package.md nord/01-05-components.md nord/01-06-frame.md nord/03-01-arm-general.md nord/03-02-arm-tm-key.md nord/03-03-arm-rx-6.md nord/03-04-arm-rxn-400.md nord/04-01-installation.md nord/04-02-smoke-zones.md nord/04-03-install-gsm.md nord/05-01-config-general.md nord/05-02-config-driver.md nord/05-03-config-version.md nord/05-04-config-info.md nord/05-05-config-toolbar.md nord/05-06-config-zones.md nord/07-01-remote-intro.md nord/07-02-remote-setup.md nord/07-03-remote-config.md nord/07-04-remote-firmware.md nord/08-01-codes-latex.md 
+pandoc --template tex/nord-template.latex -V lang=russian -o nord-manual.pdf nord/01-01-tech-desc.md nord/01-02-features.md nord/01-03-specification.md nord/01-04-package.md nord/01-05-components.md nord/01-06-frame.md nord/03-01-arm-general.md nord/03-02-arm-tm-key.md nord/03-03-arm-rx-6.md nord/03-04-arm-rxn-400.md nord/04-01-installation.md nord/04-02-smoke-zones.md nord/04-03-install-gsm.md nord/05-01-config-general.md nord/05-02-config-driver.md nord/05-03-config-version.md nord/05-04-config-info.md nord/05-05-config-toolbar.md nord/05-06-config-zones.md nord/05-12-config-radio.md nord/07-01-remote-intro.md nord/07-02-remote-setup.md nord/07-03-remote-config.md nord/07-04-remote-firmware.md nord/08-01-codes-latex.md 
 
-# pandoc --template tex/nord-template.latex -V lang=russian -o nord-manual.pdf nord/05-01-config-general.md nord/05-04-config-info.md nord/05-05-config-toolbar.md nord/05-06-config-zones.md 
+# pandoc --template tex/nord-template.latex -V lang=russian -o nord-manual.pdf nord/05-01-config-general.md nord/05-04-config-info.md nord/05-05-config-toolbar.md nord/05-06-config-zones.md nord/05-12-config-radio.md 
 
 echo "Stop to make Nord manual"
 
